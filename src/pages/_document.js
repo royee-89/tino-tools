d16e5a0986fe3ca543cad8912c7c63ee922b36e6
yehongxun <royeeee@outlook.com>
2025-04-07 01:06:29 +0800

    fix: 添加favicon.ico文件并更新引用

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,8 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         {/* 网站图标 */}
-        <link rel="icon" href="/images/logo.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/logo.png" type="image/png" />
         <link rel="apple-touch-icon" href="/images/logo.png" />
+        <meta name="msapplication-TileImage" content="/images/logo.png" />
       </Head>
       <body>
         <Main />
